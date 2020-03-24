691d84e5c5703219cc20144e064fe6db191b04c6
David Tran <davidttemail@gmail.com>
2020-03-24 17:32:37 -0600

    Update pet tables to include favorited

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200323200126) do
+ActiveRecord::Schema.define(version: 20200324223026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20200323200126) do
     t.bigint "shelter_id"
     t.string "description"
     t.string "adoption_status"
+    t.boolean "favorited"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@
 shelter_1 = Shelter.create(name: "Dumb Friends League", address: "123 Fake St", city: "Castle Rock", state: "CO", zip: "80104")
 shelter_2 = Shelter.create(name: "Pawty Please", address: "000 Another Fake St", city: "Denver", state: "CO", zip: "80204")
 
-shelter_1.pets.create(name: "Buddy", approximate_age: 6, sex: "Male", image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/single-minded-royalty-free-image-997141470-1558379890.jpg?crop=0.671xw:1.00xh;0.0847xw,0&resize=640:*", description: "Big things to come in big packages, you better be prepared to play with Buddy all day long!", adoption_status: "Adoptable")
+shelter_1.pets.create(name: "Buddy", approximate_age: 6, sex: "Male", image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/single-minded-royalty-free-image-997141470-1558379890.jpg?crop=0.671xw:1.00xh;0.0847xw,0&resize=640:*", description: "Big things to come in big packages, you better be prepared to play with Buddy all day long!", adoption_status: "Adoptable", favorited: false)
 
-shelter_2.pets.create(name: "King", approximate_age: 5, sex: "Male", image: "https://www.espree.com/sites/default/files/2019-10/DobermanPinscher.png", description: "His name might be King, but he'll treat you like royalty.", adoption_status: "Pending")
+shelter_2.pets.create(name: "King", approximate_age: 5, sex: "Male", image: "https://www.espree.com/sites/default/files/2019-10/DobermanPinscher.png", description: "His name might be King, but he'll treat you like royalty.", adoption_status: "Pending", favorited: false)
 
-shelter_1.pets.create(name: "Zula", approximate_age: 4, sex: "Female", image: "https://getyourpet.com/wp-content/uploads/2018/09/pitbull-in-phoenix-yard.jpg", description: "Just the cutest bulldog ever who loves to smile!", adoption_status: "Adoptable")
+shelter_1.pets.create(name: "Zula", approximate_age: 4, sex: "Female", image: "https://getyourpet.com/wp-content/uploads/2018/09/pitbull-in-phoenix-yard.jpg", description: "Just the cutest bulldog ever who loves to smile!", adoption_status: "Adoptable", favorited: true)
 
 shelter_1.reviews.create(title: "Best Animal Shelter", rating: 5, content: "I adopted Brownie and she was well trained. The staff are friendly and helpful.", picture:"https://m.media-amazon.com/images/M/MV5BMjg3MWFlMTQtZWNkYS00NDdiLWI4MzYtYmExYzdkMDlhMWY4XkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg")
 shelter_1.reviews.create(title: "Found My Forever Friend", rating: 4, content: "Today I brought home Simba. Very excited for him to be apart of our family.", picture:"https://www.mersive.com/wp-content/uploads/2019/07/Simba-hike-1024x768.jpg")
