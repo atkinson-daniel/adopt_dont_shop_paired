818e289776c8d5b6dc1712ea9e546690122ff8ae
David Tran <davidttemail@gmail.com>
2020-03-26 13:24:41 -0600

    Update specs to check session favorites

--- spec/features/favorites/favorites_index_spec.rb
+++ spec/features/favorites/favorites_index_spec.rb
@@ -13,29 +13,33 @@ describe "as a visitor" do
                                   sex: "Male",
                                   image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/single-minded-royalty-free-image-997141470-  1558379890.jpg?crop=0.671xw:1.00xh;0.0847xw,0&resize=640:*",
                                   description: "Big things to come in big packages, you better be prepared to play with Buddy all day long!",
-                                  adoption_status: "Adoptable",
-                                  favorited: true)
+                                  adoption_status: "Adoptable")
 
     pet_2 = shelter_1.pets.create(name: "Test_Dog_1",
                                   approximate_age: 2,
                                   sex: "Male",
                                   image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/single-minded-royalty-free-image-997141470-1558379890.jpg?crop=0.671xw:1.00xh;0.0847xw,0&resize=640:*",
                                   description: "Test Description",
-                                  adoption_status: "Adoptable",
-                                  favorited: true)
+                                  adoption_status: "Adoptable")
 
     pet_3 = shelter_1.pets.create(name: "Test_Dog_2",
                                   approximate_age: 4,
                                   sex: "Female",
                                   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRATvV2mL0wWqhpNXmsZtbsT4Zi8ElvfrE52Wvifx2C6N4P-M2S",
                                   description: "Test Description",
-                                  adoption_status: "Pending",
-                                  favorited: false)
+                                  adoption_status: "Pending")
+
+    visit "/pets/#{pet_1.id}"
+    click_on("Favorite Pet")
+
+    visit "/pets/#{pet_2.id}"
+    click_on("Favorite Pet")
 
     visit "/pets"
     expect(page).to have_no_content("Favorited Pets")
     click_on("Favorites (2)")
 
+    expect(current_path).to eq("/favorites")
     expect(page).to have_content("Favorited Pets")
     expect(page).to have_content(pet_1.name)
     expect(page).to have_content(pet_2.name)
---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Favorite do
+  describe "#total_count" do
+    it "can calculate the total number of favorited pets" do
+
+      favorites = Favorite.new(["pet_1", "pet_2"])
+
+      expect(favorites.total_count).to eq(2)
+    end
+  end
+
+  describe "#add_pet" do
+    it "can add a pet to the current session" do
+      favorites = Favorite.new(["pet_1", "pet_2"])
+
+      favorites.add_pet("pet_3")
+
+      expect(favorites.total_count).to eq(3)
+      expect(favorites.contents).to eq(["pet_1", "pet_2", "pet_3"])
+    end
+  end
+end
