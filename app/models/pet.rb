8caee3ad2a75d29e43fee279e02346b54ad09739
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-03-29 16:15:34 -0600

    Update update_adoption_status to take in additional arguments so we can use it to change the status to pending/approved=true and to adoptable/approved=false

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,9 +4,16 @@ class Pet < ApplicationRecord
   has_many :applications, through: :pet_applications
   validates_presence_of :name, :approximate_age, :sex, :image
 
-  def update_adoption_status(pet, params)
-    application = Application.find(params[:app_id])
-    pet[:adoption_status] = "Pending"
-    pet.save
+  def update_adoption_status(adoption_status, approved_status, params)
+    app = Application.find(params[:app_id])
+    pet_app = PetApplication.where(application_id: app, pet_id: self)
+    pet_app.update(approved: approved_status)
+    self[:adoption_status] = adoption_status
+    self.save
+  end
+
+  def approved_pet_app?(application, pet)
+    pet_app = application.pet_applications.where(application_id: application, pet_id: pet)
+    pet_app.first.approved
   end
 end
